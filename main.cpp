2e948a830d53b404e2a1cda304847c2503ec2580
Mauricio <91418576+mxoliveira19@users.noreply.github.com>
2023-03-22 13:28:16 +0000

    Part 1c - 10th Object and properities (UDTs)

--- main.cpp
+++ main.cpp
@@ -373,17 +373,17 @@ Define an object that is made of 5 sub-objects.
     These 5 sub-objects will not be defined using Primitives, but instead will be their own UDTs 
     you'll define these 5 sub-objects in Part 1d.
 
-Thing 10)
+Thing 10) Hospital
 5 properties:
-    1)
-    2)
-    3)
-    4)
-    5)
+    1) Doctor
+    2) ChildrenSpaceRoom
+    3) Parking
+    4) Employee
+    5) Kitchen
 3 things it can do:
-    1)
-    2)
-    3)
+    1) protect children
+    2) medicate patients
+    3) protect cars
  */
 
  /*
